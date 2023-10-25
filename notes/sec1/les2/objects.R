fb2fb3d90e09dd8f30a05933347100512226bd6e
Paul  Stanley Gebeline <psgebeli@go.olemiss.edu>
2023-10-24 20:14:39 -0500

    set up directory structure + finish sec 1.2

---
+++ notes/sec1/les2/objects.R
@@ -0,0 +1,13 @@
+# Objects are "anything stored in R", such as these variables
+# which are assigned using the assignment symbol <- (arrow)
+a <- 1
+b <- 1
+c <- -1 
+
+# Can print the variables by just writing their name, or print()
+a 
+print(a)
+
+# Can use these variables in calculations, such as the quadratic formula
+(-b + sqrt(b^2 - 4*a*c)) / (2*a)
+(-b - sqrt(b^2 - 4*a*c)) / (2*a) 
